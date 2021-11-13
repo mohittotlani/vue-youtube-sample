5e8fc49299569ff91fcecc9fc904f68384efe030
Mohit Totlani <mohit.totlani@labs108.com>
2021-11-13 23:59:51 +0530

    initial

---
+++ src/App.vue
@@ -0,0 +1,50 @@
+<template>
+  <div id="app">
+    <youtube
+      :video-id="videoId"
+      :player-vars="playerVars"
+      @playing="playing"
+      ref="youtube"
+      disablepictureinpicture
+      controlslist="nodownload"
+      fitParent
+      muted
+      @ready="onReady"
+      @cued="cued"
+      @paused="paused"
+    />
+  </div>
+</template>
+
+<script>
+export default {
+  name: "app",
+  computed: {
+    player() {
+      return this.$refs.youtube.player;
+    },
+  },
+  data() {
+    return {
+      videoId: "rstRFJfBkWo",
+      playerVars: {
+        autoplay: 0,
+      },
+    };
+  },
+  methods: {
+    playing() {
+      console.log("o/ we are watching!!!");
+    },
+    onReady() {
+      console.log("o/ I am ready!!!");
+    },
+    cued() {
+      console.log("o/ we are cued!!!");
+    },
+    paused() {
+      console.log("o/ I am paused!!!");
+    },
+  },
+};
+</script>
---
+++ src/main.js
@@ -0,0 +1,11 @@
+import Vue from "vue";
+import App from "./App.vue";
+
+import VueYoutube from "vue-youtube";
+
+Vue.use(VueYoutube);
+Vue.config.productionTip = false;
+
+new Vue({
+  render: (h) => h(App)
+}).$mount("#app");
